96af6e1354c48b2e80e4d4da7db35735b5971e30
Mohamed Fathy <codetwostart@gmail.com>
2019-10-12 14:23:07 +0200

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace dowhileloop
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+           int counter = 5;
+           //while execute zero or more times
+           //check then execute
+          /*  while(counter < 5){
+               Console.WriteLine("{0}- Hello", counter + 1);
+               counter++;
+           } */
+          do{
+               Console.WriteLine("{0}- Hello", counter + 1);
+               counter++;
+           } while(counter < 5);
+        }
+    }
+}
